131d68313b549063947b98c026b9de6202bc6fa0
Aiyan Tufail <aiyantufail@hotmail.com>
2024-06-01 23:08:08 +0500

    Header first draft complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,21 @@
 import '../styles/header.css'
 import Palette from '../assets/icons/palette.png'
 import Search from '../assets/icons/search-icon.png'
+import Cart from '../assets/icons/cart.png'
 
 export default function Header() {
     return (
         <>
             <header className="header">
 
-                <div className='left-section'>
-                    <img className='palette-icon' src={Palette} alt="Palette icon" />
-                    <p className='title'>ArtCart</p>
-                </div>
+                <div className='top-section'>
 
-                <div className='middle-section'>
-                    <div className='categories'>
+                    <div className='top-left-section'>
+                        <img className='palette-icon' src={Palette} alt="Palette icon" />
+                        <p className='title'>ArtCart</p>
+                    </div>
+
+                    <div className='top-middle-section'>
                         <p>Paintings</p>
                         <p>Sketches</p>
                         <p>Digital Art</p>
@@ -21,14 +23,24 @@ export default function Header() {
                         <p>Crafts</p>
                     </div>
 
-                    <div className='search-bar-search-icon'>
-                        <input className='search-bar' type="text" placeholder='Search for artworks, artists, articles...' />
-                        <img  className='search-icon' src={Search} alt="Search icon" />
+                    <div className='top-right-section'>
+                        <div className='top-right-left-section'>
+                            <p>Log In</p>
+                            <p>|</p>
+                            <p>Sign Up</p>
+                        </div>
+
+                        <img className='cart-icon' src={Cart} alt="Cart icon"/>
                     </div>
+
                 </div>
 
-
-                <div className='right-section'></div>
+                <div className='bottom-section'>
+                    <div className='search-bar-search-icon'>
+                        <input className='search-bar' placeholder='Search for artworks, artists, articles...' type="text" />
+                        <img className='search-icon' src={Search} alt="Search icon" />
+                    </div>
+                </div>
             </header>
         </>
     )
